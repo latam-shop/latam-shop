cfc66bc1399f1216e443773cf56609708aacb925
HoyosDiego <dfhoyos29@gmail.com>
2026-03-25 22:05:07 -0500

    feat(app): icon Atom

---
+++ src/components/atoms/Icons/icon.tsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProps } from './icon.types';
+import { ICON_LIBRARY } from './icon.util';
+
+const Icon = ({
+    name,
+    color = 'text-gray-900',
+    size = 'xl',
+    filled = true
+}: IconProps) => {
+    const iconSet = ICON_LIBRARY[name];
+
+    if (!iconSet) {
+        return null;
+    }
+    const selectedIcon = filled ? iconSet.solid : iconSet.regular;
+
+    return (
+        <FontAwesomeIcon
+            size={size}
+            icon={selectedIcon}
+            className={`${color} shrink-0 block`}
+            aria-hidden="true"
+        />
+    );
+};
+
+export default Icon;
